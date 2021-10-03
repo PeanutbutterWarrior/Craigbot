4338076d050da16d93a3dbdaec028de12c6ae0f0
PeanutbutterWarrior <50717143+PeanutbutterWarrior@users.noreply.github.com>
2021-10-03 11:04:35 +0100

    Change embarrass to use the webhook manager

--- commands/embarrass.py
+++ commands/embarrass.py
@@ -6,8 +6,8 @@ with open(os.path.normpath('commands/embarrassing_phrases.txt')) as file:
     embarrassing_phrases = file.read().splitlines()
 
 
-async def embarrass(message: discord.Message):
-    webhook = await message.channel.create_webhook(name='Craighook')
+async def embarrass(message: discord.Message, webhook_manager):
+    webhook = await webhook_manager.get_webhook(message.channel)
     await webhook.send(random.choice(embarrassing_phrases),
                        username=message.author.display_name,
                        avatar_url=message.author.avatar_url)
