686bc5f83277e85cdedbd3f0ff2545aef117e596
PeanutbutterWarrior <50717143+PeanutbutterWarrior@users.noreply.github.com>
2021-10-18 19:51:31 +0100

    Turn client into custom class

--- commands.py
+++ commands.py
@@ -3,29 +3,32 @@ import os.path
 import random
 
 import helpers
+from main import CustomClient
 
 commands = {}
 
 
 class Command:
-    def __init__(self, function, name, description, usage):
+    def __init__(self, function, name, description, explanation, usage):
         self.name = name
         self.description = description
         self.usage = usage
         self.function = function
+        self.explanation = explanation
 
-    async def __call__(self, message: discord.Message, webhook_manager: helpers.WebhookManager, *args):
-        if helpers.WebhookManager in self.function.__annotations__.values():
-            await self.function(message, webhook_manager, *args)
-        else:
-            await self.function(message, *args)
+    async def __call__(self, message: discord.Message, client: CustomClient, *args):
+        await self.function(message, client, *args)
 
 
-def command(name, description, usage=''):
+def command(name, description, explanation=None, usage=''):
+    if explanation is None:
+        explanation = description
+
     def wrapper(func):
-        to_return = Command(func, name, description, name + usage)
+        to_return = Command(func, name, description, explanation, name + usage)
         commands[name] = to_return
         return to_return
+
     return wrapper
 
 
@@ -44,48 +47,53 @@ with open('data/advice.txt') as file:
 
 @command(name='embarrass',
          description='Embarrass yourself or others',
+         explanation='Send an embarrassing message as you, or another person. Provide a person by @pinging them.',
          usage=' [person]')
-async def embarrass(message: discord.Message, webhook_manager: helpers.WebhookManager, *args):
-    webhook = await webhook_manager.get_webhook(message.channel)
-    await webhook.send(random.choice(embarrassing_phrases),
-                       username=message.author.display_name,
-                       avatar_url=message.author.avatar_url)
+async def embarrass(message: discord.Message, client: CustomClient, *args):
+    if len(args) == 0:
+        webhook = await client.webhook_manager.get_webhook(message.channel)
+        await webhook.send(random.choice(embarrassing_phrases),
+                           username=message.author.display_name,
+                           avatar_url=message.author.avatar_url)
+    else:
+        message.channel.send('This isn\'t implemented yet. Maybe in the next version')
+        raise NotImplemented
 
 
 @command(name='funfact',
-         description='Get a fun fact from Craig')
-async def funfact_command(message: discord.Message, *args):
+         description='Get a fun fact from Craig',
+         explanation='Get a fun fact about Home Depot:tm: from Craig')
+async def funfact_command(message: discord.Message, client: CustomClient, *args):
     await message.channel.send(random.choice(fun_facts))
 
 
 @command(name='dadjoke',
          description='Have Craig tell you a dad joke')
-async def dadjoke_command(message: discord.Message, *args):
+async def dadjoke_command(message: discord.Message, client: CustomClient, *args):
     await message.channel.send(random.choice(dad_jokes))
 
 
 @command(name='advice',
          description='Get Craig\'s advice')
-async def advice_command(message: discord.Message, *args):
+async def advice_command(message: discord.Message, client: CustomClient, *args):
     await message.channel.send(random.choice(advice))
 
 
 @command(name='help',
-         description='Get general help or help for a specific command',
+         description='This help text',
+         explanation='This command is used to view information about all commands, or detailed information about a command',
          usage=' [command]')
-async def help_command(message: discord.Message, *args):
-    if len(args) == 0:
-        help_text = f'''**Craig** v1.0.0
-
-Commands:
-    '''
+async def help_command(message: discord.Message, client: CustomClient, *args):
+    try:
+        command = commands[args[0]]
+    except (KeyError, IndexError):
+        help_text = f'Craig _v{client.version}_\n\nCommands:'
         command_list = []
-        for command in commands.values():
-            command_list.append(f'**{command.name}**: {command.description}')
-        help_text += '\n    '.join(command_list)
-        help_text += '''
-Use c!help (command) to get help for a specific command
-'''
+        for command in sorted(commands.values(), key=lambda i: i.name):
+            command_list.append(f'\n    {client.prefix}**{command.name}**: {command.description}')
+        help_text += ''.join(command_list)
+        help_text += '\n\nUse c!help [command] to get help for a specific command'
         await message.channel.send(help_text)
     else:
-        ...
+        help_text = f'{client.prefix}**{command.name}**\n{command.explanation}'
+
--- main.py
+++ main.py
@@ -5,12 +5,6 @@ import re
 import commands
 import helpers
 
-
-bot_prefix = 'c!'
-
-im_pattern = re.compile('i\'?m (.+)', re.IGNORECASE)
-is_cool_pattern = re.compile('([^ ]+) (?:is|are) cool')
-
 shut_ups = {'shut up',
             'shut your up',
             'stfu',
@@ -19,56 +13,63 @@ shut_ups = {'shut up',
             'shut the hell your mouth',
             'shut the hell your up'}
 
-client = discord.Client()
-webhook_manager = helpers.WebhookManager()
+
+class CustomClient(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.prefix = 'c!'
+        self._im_pattern = re.compile('i\'?m (.+)', re.IGNORECASE)
+        self._is_cool_pattern = re.compile('([^ ]+) (?:is|are) cool', re.IGNORECASE)
+        self.webhook_manager = helpers.WebhookManager()
+        self.version = '1.0.0'
+        
+    async def on_ready(self):
+        await self.webhook_manager.init(self)
+        print(f'{self.user} has connected to Discord!')
+
+    async def on_message(self, message: discord.Message):
+        if message.author == self.user:
+            return
+        if message.author.bot:
+            return
+
+        if message.content.startswith(self.prefix):
+            await self.call_command(message)
+
+        if message.content == 'Hi craig':
+            await message.channel.send(f'Hi {message.author.display_name}')
+        # Checks for I'm ------
+        elif (regex_match := self._im_pattern.search(message.content)) is not None:
+            if regex_match.group(1).lower() == 'craig':
+                await message.channel.send('You\'re not Craig, I\'m Craig!')
+            else:
+                await message.channel.send(f'Hi {regex_match.group(1)}, I\'m Craig!')
+        # Checks for shut up
+        elif any(shut_up in message.content.lower() for shut_up in shut_ups):
+            await message.channel.send(f'Listen here {message.author.display_name}, I will not tolerate you saying those '
+                                       f'bloody words that consist of the letters \'s h u t  u p\' in this server, '
+                                       f'so take your own advice and close thine god damn mouth in the name of the '
+                                       f'christian minecraft server owner!')
+        # Checks for ----- is cool
+        elif (regex_match := self._is_cool_pattern.search(message.content)) is not None:
+            await message.channel.send(f'{regex_match.group(1).capitalize()} may be cool, but Home Depot is cooler. We sell '
+                                       f'top of the line tools, amazing DIY construction products, and premium services '
+                                       f'for the discerning customer. Come down today to buy a new drill, or a razor '
+                                       f'sharp saw. We might even sell {regex_match.group(1).lower()}!')
+
+    async def call_command(self, message: discord.Message):
+        command_name, *arguments = message.content[len(self.prefix):].split(' ')
+        if command_name in commands.commands:
+            await commands.commands[command_name](message, self, *arguments)
 
 
-async def call_command(message_content, message):
-    command_name, *arguments = message_content[len(bot_prefix):].split(' ')
-    if command_name in commands.commands:
-        await commands.commands[command_name](message, webhook_manager, *arguments)
+if __name__ == '__main__':
+    with open('.env') as env_file:
+        for line in env_file.read().split('\n'):
+            key, val = line.split('=')
+            os.environ[key] = val
 
+    TOKEN = os.getenv('DISCORD_TOKEN')
 
-@client.event
-async def on_ready():
-    await webhook_manager.init(client)
-    print(f'{client.user} has connected to Discord!')
-
-
-@client.event
-async def on_message(message: discord.Message):
-    if message.author == client.user:
-        return
-    if message.author.bot:
-        return
-
-    if message.content.startswith(bot_prefix):
-        await call_command(message.content, message)
-
-    if message.content == 'Hi craig':
-        await message.channel.send(f'Hi {message.author.display_name}')
-    elif (regex_match := im_pattern.search(message.content)) is not None:
-        if regex_match.group(1).lower() == 'craig':
-            await message.channel.send('You\'re not Craig, I\'m Craig!')
-        else:
-            await message.channel.send(f'Hi {regex_match.group(1)}, I\'m Craig!')
-    elif any(shut_up in message.content for shut_up in shut_ups):
-        await message.channel.send(f'Listen here {message.author.display_name}, I will not tolerate you saying those '
-                                   f'bloody words that consist of the letters \'s h u t  u p\' in this server, '
-                                   f'so take your own advice and close thine god damn mouth in the name of the '
-                                   f'christian minecraft server owner!')
-    elif (regex_match := is_cool_pattern.search(message.content)) is not None:
-        await message.channel.send(f'{regex_match.group(1).capitalize()} may be cool, but Home Depot is cooler. We sell '
-                                   f'top of the line tools, amazing DIY construction products, and premium services '
-                                   f'for the discerning customer. Come down today to buy a new drill, or a razor '
-                                   f'sharp saw. We might even sell {regex_match.group(1).lower()}!')
-
-
-with open('.env') as env_file:
-    for line in env_file.read().split('\n'):
-        key, val = line.split('=')
-        os.environ[key] = val
-
-TOKEN = os.getenv('DISCORD_TOKEN')
-
-client.run(TOKEN)
+    client = CustomClient()
+    client.run(TOKEN)
